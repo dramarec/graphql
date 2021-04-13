53adb6d81aa415d99c4a9bd8e5628dc9ff2db9a6
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-13 10:21:51 +0300

    addd apollo client

---
+++ frontend/src/app/index.js
@@ -0,0 +1,20 @@
+// Core
+import React from "react";
+// import { ApolloProvider } from "@apollo/react-hooks";
+
+// Components
+// import { Pet } from './bus/pet';
+
+// Other
+// import { client } from './init/client';
+
+export const App = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+        // <ApolloProvider client={client}>
+        //   <Pet />
+        // </ApolloProvider>
+    );
+};
