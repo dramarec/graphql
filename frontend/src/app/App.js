40ef93683e23754345f8c7751e1bb8a180497bb0
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-15 12:51:18 +0300

    added auth with subscriptions

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -3,22 +3,45 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 // Components
-// import { Customer } from './bus/customer/auth';
-import { Login } from './bus/customer/login';
-import { Pet } from './bus/pet';
+// import { Pet } from './bus/pet';
+// import { Customer } from './bus/customer';
+// import { Login } from './bus/customer/login';
+import { User } from './bus/user';
 
 // Other
-import { client } from './init/client';
+import { client } from './init/modern';
 
 export const App = () => {
     return (
         <ApolloProvider client={client}>
-            <h1>Hello graphQL</h1>
-
-            {/* <Customer /> */}
-            <Login />
-
-            <Pet />
+            {/*<Customer />*/}
+            {/*<Login />*/}
+            {/*<Pet />*/}
+            <User />
         </ApolloProvider>
     );
 };
+// // Core
+// import React from 'react';
+// import { ApolloProvider } from '@apollo/react-hooks';
+
+// // Components
+// // import { Customer } from './bus/customer/auth';
+// import { Login } from './bus/customer/login';
+// import { Pet } from './bus/pet';
+
+// // Other
+// import { client } from './init/client';
+
+// export const App = () => {
+//     return (
+//         <ApolloProvider client={client}>
+//             <h1>Hello graphQL</h1>
+
+//             {/* <Customer /> */}
+//             <Login />
+
+//             <Pet />
+//         </ApolloProvider>
+//     );
+// };
