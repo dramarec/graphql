23749137bb82767f6922b726467833ede833f922
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-15 19:36:53 +0300

    added mutation

--- backend/src/model.js
+++ backend/src/model.js
@@ -1,12 +1,69 @@
-const booksDB = [
-    {
-        title: 'Harry Potter',
-        author: 'J.K. Rowling'
-    },
+//!
+// const initialDB = [
+//     {
+//         title: 'Harry Potter',
+//         author: 'J.K. Rowling',
+//         id: 'ID-1'
+//     },
 
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton'
-    }
+//     {
+//         title: 'Jurassic Park',
+//         author: 'Michael Crichton',
+//         id: 'ID-1'
+//     }
+// ];
+// export const getBooks = () => initialDB;
+// console.log('getBooks', getBooks());
+//===============================
+
+const initialDB = [
+    [
+        'ID-1',
+        {
+            title: 'Harry Potter',
+            author: 'J.K. Rowling'
+        }
+    ],
+    [
+        'ID-2',
+        {
+            title: 'Jurassic Park',
+            author: 'Michael Crichton'
+        }
+    ]
 ];
-export const getBooks = () => booksDB;
+
+const booksDB = new Map(initialDB);
+
+// =======  getBooks
+export const getBooks = () => {
+    const books = [];
+
+    booksDB.forEach((value, key) => {
+        // console.log('booksDB.forEach ===> key', key);
+        // console.log('booksDB.forEach ===> value', value);
+        const currentBook = {
+            id: key,
+            ...value
+        };
+
+        books.push(currentBook);
+    });
+    // console.log('booksDB.forEach ===> books', books);
+    return books;
+};
+// console.log('getBooks ===> getBooks()', getBooks());
+
+// ===
+export const getBookById = id => {
+    const book = booksDB.get(id);
+    return { id, ...book };
+};
+
+export const saveBook = (title, id, author) => {
+    const receivedBook = { title, author };
+    booksDB.set(id, receivedBook);
+    const savedBook = booksDB.get(id);
+
+    return { id, ...savedBook };
+};
--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -2,6 +2,16 @@ import { getBooks, getBookById, saveBook, removeBook, updateBook } from './model
 
 export const resolvers = {
     Query: {
-        books: () => getBooks()
+        books: () => getBooks(),
+        book: (_, { id }) => {
+            const book = getBookById(id);
+            console.log('book', book);
+            return book;
+        }
+    },
+    Mutation: {
+        addBook: (_, { title, id, author }) => {
+            return saveBook(title, id, author);
+        }
     }
 };
